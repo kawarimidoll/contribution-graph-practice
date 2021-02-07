4858f528e53131b8b64a3c6d7191b5ff9ebf8cd0
kawarimidoll <kawarimidoll+git@gmail.com>
2021-02-07 12:11:35 +0900

    add legend

--- src/Svg.svelte
+++ src/Svg.svelte
@@ -9,9 +9,22 @@
   const rectStep = rectSize + rectSpan;
   const rectStroke = "rgba(27,31,35,0.06)";
   const rectStrokeWidth = 2;
+  const getRectAttrs = (x, y, fill) => ({
+    x: x * rectStep,
+    y: y * rectStep,
+    fill: `#${fill}`,
+    width: rectSize,
+    height: rectSize,
+    rx: rectRadius,
+    ry: rectRadius,
+    stroke: rectStroke,
+    "stroke-width": rectStrokeWidth,
+  });
+
+  const fontSize = rectSize + rectSpan;
 
   const width = rectStep * (weeks.length + 2) - rectSpan;
-  const height = rectStep * (days.length + 2) - rectSpan;
+  const height = rectStep * (days.length + 2) - rectSpan + fontSize;
 </script>
 
 <svg {width} {height}>
@@ -19,18 +32,19 @@
   <g transform="translate({rectStep}, {rectStep})">
     {#each weeks as week}
       {#each days as day}
-        <rect
-          x={week * rectStep}
-          y={day * rectStep}
-          width={rectSize}
-          height={rectSize}
-          rx={rectRadius}
-          ry={rectRadius}
-          fill="#{colors[0]}"
-          stroke={rectStroke}
-          stroke-width={rectStrokeWidth}
-        />
+        <rect {...getRectAttrs(week, day, colors[0])} />
       {/each}
+    {/each}
+  </g>
+
+  <g transform="translate({rectStep}, {height - rectSize})">
+    <text font-size={fontSize}>
+      <a href="https://github.com/kawarimidoll">@kawarimidoll</a>
+    </text>
+  </g>
+  <g transform="translate({width - rectStep * 7}, {height - rectSize * 2})">
+    {#each colors as color, i}
+      <rect {...getRectAttrs(i, 0, color)} />
     {/each}
   </g>
 </svg>
